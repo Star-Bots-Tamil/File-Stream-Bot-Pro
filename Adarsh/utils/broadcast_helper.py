d7aa444597eb4278d98389d6ba4c5da376ec2906
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-08-22 05:04:46 +0530

    Update broadcast_helper.py

--- Adarsh/utils/broadcast_helper.py
+++ Adarsh/utils/broadcast_helper.py
@@ -3,11 +3,17 @@
 import asyncio
 import traceback
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
+from WebStreamer.vars import Var
+broadcast_ids = {}
 
+BROADCAST_AS_COPY = Var.BROADCAST_AS_COPY
 
 async def send_msg(user_id, message):
     try:
-        await message.forward(chat_id=user_id)
+        if BROADCAST_AS_COPY is False:
+            await message.forward(chat_id=user_id)
+        elif BROADCAST_AS_COPY is True:
+            await message.copy(chat_id=user_id)
         return 200, None
     except FloodWait as e:
         await asyncio.sleep(e.x)
